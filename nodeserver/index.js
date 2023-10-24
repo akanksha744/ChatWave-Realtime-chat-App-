16b596cd8fa170395bb2086454cd62d5059dbd0e
akanksha744 <akankshamishra669@gmail.com>
2023-10-24 13:22:41 +0530

    changes

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -1,21 +1,32 @@
-const io = require('socket.io') (8000)
+const server = require('http').createServer();
+const io = require('socket.io')(server, {
+  cors: {
+    origin: "*",
+  }
+});
 
 const users = {};
 
-io.on('connection', socket =>{
-    socket.on('new-user-joined', userName =>{
-        users[socket.id] = userName;
-        socket.broadcast.emit('user-joined', userName);
+io.on('connection', (socket) => {
+  socket.on('new-user-joined', (userName) => {
+    users[socket.id] = userName;
+    socket.broadcast.emit('user-joined', userName);
+  });
 
-    });
-    socket.on('send', message =>{
-        socket.broadcast.emit('receive',{message: message, userName:users[socket.id]})
-    });
-    socket.on('disconnect', message =>{
-        socket.broadcast.emit('left',users[socket.id]);
-        delete users[socket.id];
+  socket.on('send', (message) => {
+    socket.broadcast.emit('receive', { message, userName: users[socket.id] });
+  });
 
-    });
+  socket.on('disconnect', () => {
+    const userName = users[socket.id];
+    if (userName) {
+      socket.broadcast.emit('left', userName);
+      delete users[socket.id];
+    }
+  });
+});
 
-
-})
+const port = 8000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
