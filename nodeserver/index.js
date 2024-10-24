b7dce5013b1ed44a6dff80faacdaa295e763fdca
akanksha744 <136923367+akanksha744@users.noreply.github.com>
2024-10-24 11:50:06 +0530

    Update index.js

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -8,31 +8,34 @@ const server = http.createServer(app);
 const io = new Server(server);
 const users = {};
 
-// Socket.io
+// Serving static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle root URL
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// WebSocket connection handling
 io.on("connection", (socket) => {
+    console.log(`New connection: ${socket.id}`);
+
     socket.on("new-user-joined", (userName) => {
         users[socket.id] = userName;
-        io.emit("user-joined", userName); 
+        io.emit("user-joined", userName);
     });
 
-    socket.on('send', (message) => {
-        io.emit('receive', { message: message, userName: users[socket.id] }); 
+    socket.on("send", (message) => {
+        io.emit("receive", { message, userName: users[socket.id] });
     });
 
-    socket.on('disconnect', () => {
+    socket.on("disconnect", () => {
         const userName = users[socket.id];
         delete users[socket.id];
-        io.emit('left', userName);
+        io.emit("left", userName);
     });
 });
 
-
-app.use(express.static(path.resolve("../")));
-
-
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve("index.html"));
-});
-
-
-server.listen(9000, () => console.log(`Server Started at PORT:9000`));
+// Start the server
+const PORT = 9000;
+server.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
